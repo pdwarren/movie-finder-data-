00a1f0b2a0f77ba9025ed272341e37e05c677ef2
Parker Warren <parker.warren19@gmail.com>
2024-12-17 13:01:28 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const morgan = require('morgan');
 const NodeCache = require('node-cache');
+require('dotenv').config();
 
 // Initialize Express and Cache
 const app = express();
@@ -18,9 +18,14 @@ if (!OMDB_API_KEY) {
     process.exit(1);
 }
 
+const OMDB_API_URL = process.env.OMDB_API_URL;
+// const OMDB_API_KEY = process.env.OMDB_API_KEY;
+
 // Endpoint to handle movie data requests
 app.get('/', async (req, res) => {
-    let path = req.url
+    // let path = req.url
+
+    // console.log(path)
     const { i, t } = req.query;
     if (!i && !t) {
         return res.status(400).send('Query parameter "i" (IMDB ID) or "t" (movie title) is required.');
@@ -28,10 +33,36 @@ app.get('/', async (req, res) => {
 
     const cacheKey = i ? `i:${i}` : `t:${t.toLowerCase()}`;
     const cachedData = cache.get(cacheKey);
+
     if (cachedData) {
         console.log(`Cache hit for ${cacheKey}`);
         return res.json(cachedData);
-    }  
+    }
+
+
+    try {
+
+        const response = await axios.get(OMDB_API_URL, {
+            params: {
+                apikey: OMDB_API_KEY,
+                i,
+                t
+            }
+        })
+
+        if (response.data.Response === 'False') {
+            return res.status(404).send(response.data.Error);
+        }
+
+        cache.set(cacheKey, response.data);
+        console.log(`Cache miss for ${cacheKey}. Data fetched and cached.`);
+        res.json(response.data)
+
+    } catch (error) {
+        console.error('Error fetching data from OMDb:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+
 
     // try {
     //     const response = await axios.get('http://www.omdbapi.com/', {
@@ -41,28 +72,29 @@ app.get('/', async (req, res) => {
     //     if (response.data.Response === 'False') {
     //         return res.status(404).send(response.data.Error);
     //     }
-    axios
-        .get('http://www.omdbapi.com' + path + '&apikey=2e588a3')
-        .then(function (response) {
-            let data = response.data;
+    // console.log(path)
+    // axios
+    //     .get('http://www.omdbapi.com' + path + '&apikey=3cc4c00e')
+    //     .then(function (response) {
+    //         let data = response.data;
 
-            cache[path] = data;
-            res.status(200).json(data);
-        })
-        .catch(function (error) {
-            res.status(200).json(error.message);
-        })
-    
+    //         cache[path] = data;
+    //         res.status(200).json(data);
+    //     })
+    //     .catch(function (error) {
+    //         res.status(200).json(error.message);
+    //     })
 
-        // Store data in cache for future requests
-        cache.set(cacheKey, response.data);
-        console.log(`Cache miss for ${cacheKey}. Data fetched and cached.`);
-        res.json(response.data);
+
+    //     // Store data in cache for future requests
+    //     cache.set(cacheKey, response.data);
+    //     console.log(`Cache miss for ${cacheKey}. Data fetched and cached.`);
+    //     res.json(response.data);
     // } catch (error) {
     //     console.error('Error fetching data from OMDb:', error.message);
     //     res.status(500).send('Internal Server Error');
     // }
-;
+    ;
 });
 
 module.exports = app;
